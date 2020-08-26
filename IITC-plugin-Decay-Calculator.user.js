f2e1d36aadd9f05aabcfab34d7ec3cace291eef8
Ayaro <Ayaro1@users.noreply.github.com>
2020-08-26 22:13:14 +0200

    Update IITC-plugin-Decay-Calculator.user.js

--- IITC-plugin-Decay-Calculator.user.js
+++ IITC-plugin-Decay-Calculator.user.js
@@ -1,147 +1,157 @@
 // ==UserScript==
-// @id             iitc-plugin-decay-calculator@Zaso
+// @author         Zaso
 // @name           IITC plugin: Decay Calculator
 // @category       Info
-// @version        0.0.3.20200707.1
-// @namespace      http://www.giacintogarcea.com/ingress/items/
-// @updateURL      https://github.com/Ayaro1/IITC-plugin-Decay-Calculator-Fan-Update/raw/master/IITC-plugin-Decay-Calculator.user.js
-// @downloadURL    https://github.com/Ayaro1/IITC-plugin-Decay-Calculator-Fan-Update/raw/master/IITC-plugin-Decay-Calculator.user.js
-// @description    Estimate the portal decaying. Fan update by Ayaro
-// @include        *://*.ingress.com/intel*
-// @include        *://*.ingress.com/mission/*
-// @include        *://intel.ingress.com/*
-// @match          *://*.ingress.com/intel*
-// @match          *://*.ingress.com/mission/*
-// @match          *://intel.ingress.com/*
+// @version        0.0.4.20200826.2208
+// @description    Estimate portal decay
+// @id             decay-calculator
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @downloadURL    https://github.com/MysticJay/ZasoItems.CE/raw/master/decay-calculator.user.js
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
 
-function wrapper(){
-	// ensure plugin framework is there, even if iitc is not yet loaded
-	if(typeof window.plugin !== 'function') window.plugin = function(){};
+function wrapper(plugin_info) {
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
 
-	// PLUGIN START ////////////////////////////////////////////////////////
+//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+//(leaving them in place might break the 'About IITC' page or break update checks)
+plugin_info.buildName = 'ZasoItems';
+plugin_info.dateTimeVersion = '2020-08-26-2208';
+plugin_info.pluginId = 'decay-calculator';
+//END PLUGIN AUTHORS NOTE
 
-		// use own namespace for plugin
-		window.plugin.decayCalculator = function() {};
 
-		window.plugin.decayCalculator.generateArrayEnergy = function(data){
-			var d = data.portalDetails;
 
-			var l,v,max,perc,perc2;
-			var list = [];
+// PLUGIN START ////////////////////////////////////////////////////////
+// History
+// 0.0.4 moving some constants
+// 0.0.3 Headers changed. Ready for IITC-CE
+// 0.0.2 Original sript
 
-			for(var i=0;i<8;i++){
-				var reso = d.resonators[i];
-				if(reso){
-					l = parseInt(reso.level);
-					v = parseInt(reso.energy);
-					max = RESO_NRG[l];
-					perc = Math.round((v/max * 100));
-					perc2 = v/max * 100;
-				} else {
-					l = 0;
-					v = 0;
-					max = 0;
-					perc = 0;
-					perc2 = 0;
-				}
 
-				list.push(perc);
-			}
-			return list.sort();
-		}
-		window.plugin.decayCalculator.generateArrayDays = function(risArray){
-			list = [];
+  // use own namespace for plugin
+  window.plugin.decayCalculator = function() {};
+  window.plugin.decayCalculator.decayRate = 5; // percent of resonator's capacity per day
 
-			for(i in risArray){
-				var gg = window.plugin.decayCalculator.risDecay(risArray[i]);
-				list.push(gg);
-			}
-			return list.sort();
-		}
+  window.plugin.decayCalculator.generateArrayEnergy = function(data){
+    var d = data.portalDetails;
 
-		window.plugin.decayCalculator.risMin = function(risArray){
-			var min = 0;
-			for(i in risArray){
-				min = Math.min(min, risArray[i]);
-			}
-			return(min);
-		}
-		window.plugin.decayCalculator.risMax = function(risArray){
-			var max = 0;
-			for(i in risArray){
-				max = Math.max(max, risArray[i]);
-			}
-			return(max);
-		}
+    var l,v,max,perc,perc2;
+    var list = [];
 
-		window.plugin.decayCalculator.risDecay = function(ris){
-			return Math.ceil(ris/5);
-		}
+    for(var i=0;i<8;i++){
+      var reso = d.resonators[i];
+      if(reso){
+        l = parseInt(reso.level);
+        v = parseInt(reso.energy);
+        max = RESO_NRG[l];
+        perc = Math.round((v/max * 100));
+        perc2 = v/max * 100;
+      } else {
+        l = 0;
+        v = 0;
+        max = 0;
+        perc = 0;
+        perc2 = 0;
+      }
 
-		window.plugin.decayCalculator.portalNeut = function(risArray){
-			var max = window.plugin.decayCalculator.risMax(risArray);
-			var gg = window.plugin.decayCalculator.risDecay(max);
+      list.push(perc);
+    }
+    return list.sort();
+  }
+  window.plugin.decayCalculator.generateArrayDays = function(risArray){
+    list = [];
 
-			return(gg);
-		}
-		window.plugin.decayCalculator.linkNeut = function(risArray){
-			arrDays = window.plugin.decayCalculator.generateArrayDays(risArray);
-			var gg = window.plugin.decayCalculator.risDecay(risArray[5]);
-			return gg;
-		}
+    for(i in risArray){
+      var gg = window.plugin.decayCalculator.risDecay(risArray[i]);
+      list.push(gg);
+    }
+    return list.sort();
+  }
 
-		//---------------------------------------------------------------------------------------
+  window.plugin.decayCalculator.risMin = function(risArray){
+    var min = 0;
+    for(i in risArray){
+      min = Math.min(min, risArray[i]);
+    }
+    return(min);
+  }
+  window.plugin.decayCalculator.risMax = function(risArray){
+    var max = 0;
+    for(i in risArray){
+      max = Math.max(max, risArray[i]);
+    }
+    return(max);
+  }
 
-		window.plugin.decayCalculator.appendDetails = function(data){
-			window.plugin.decayCalculator.appendDetails2(data);
-		}
+  window.plugin.decayCalculator.risDecay = function(ris){
+    return Math.ceil(ris/window.plugin.decayCalculator.decayRate);
+  }
 
-		window.plugin.decayCalculator.appendDetails2 = function(data){
-			risArray = window.plugin.decayCalculator.generateArrayEnergy(data);
+  window.plugin.decayCalculator.portalNeut = function(risArray){
+    var max = window.plugin.decayCalculator.risMax(risArray);
+    var gg = window.plugin.decayCalculator.risDecay(max);
 
-			fd = window.plugin.decayCalculator.risDecay(risArray[0]);
-			ld = window.plugin.decayCalculator.linkNeut(risArray);
-			pd = window.plugin.decayCalculator.portalNeut(risArray);
+    return(gg);
+  }
+  window.plugin.decayCalculator.linkNeut = function(risArray){
+    arrDays = window.plugin.decayCalculator.generateArrayDays(risArray);
+    var gg = window.plugin.decayCalculator.risDecay(risArray[5]);
+    return gg;
+  }
 
-			var html = '';
-			html += '<div>Incomplete in<br/>max <b>'+fd+'</b> day(s)</div>';
-			html += '<div>Links expires in<br/>max <b>'+ld+'</b> day(s)</div>';
-			html += '<div>Neutral in<br/>max <b>'+pd+'</b> day(s)</div>';
+  //---------------------------------------------------------------------------------------
 
-			$('#portaldetails .linkdetails').before('<div class="decayCalculator">'+html+'</div>');
-		}
+  window.plugin.decayCalculator.appendDetails = function(data){
+    window.plugin.decayCalculator.appendDetails2(data);
+  }
 
-		//---------------------------------------------------------------------------------------
-		// Append the stylesheet
-		//---------------------------------------------------------------------------------------
-		window.plugin.decayCalculator.setupCSS = function(){
-			$('<style>').prop('type', 'text/css').html(''
-				+'.decayCalculator{text-align:center;padding:2px 0 3px;border:1px solid #20A8B1;border-width:1px 0;}'
-				+'.decayCalculator div{display:inline-block;width:33%;color:lightgrey;}'
-				+'.decayCalculator div b{color:#ffce00;}'
-			).appendTo('head');
-		}
+  window.plugin.decayCalculator.appendDetails2 = function(data){
+    risArray = window.plugin.decayCalculator.generateArrayEnergy(data);
 
-		var setup = function() {
-			window.plugin.decayCalculator.setupCSS();
+    fd = window.plugin.decayCalculator.risDecay(risArray[0]);
+    ld = window.plugin.decayCalculator.linkNeut(risArray);
+    pd = window.plugin.decayCalculator.portalNeut(risArray);
 
-			window.addHook('portalDetailsUpdated', window.plugin.decayCalculator.appendDetails);
-		};
+    var html = '';
+    html += '<div>Incomplete in<br/>max <b>'+fd+'</b> day(s)</div>';
+    html += '<div>Links expires in<br/>max <b>'+ld+'</b> day(s)</div>';
+    html += '<div>Neutral in<br/>max <b>'+pd+'</b> day(s)</div>';
 
-	// PLUGIN END //////////////////////////////////////////////////////////
+    $('#portaldetails .linkdetails').before('<div class="decayCalculator">'+html+'</div>');
+  }
 
-	if(window.iitcLoaded && typeof setup === 'function'){
-		setup();
-	}else{
-		if(window.bootPlugins)
-			window.bootPlugins.push(setup);
-		else
-			window.bootPlugins = [setup];
-	}
-	} // wrapper end
-	// inject code into site context
-	var script = document.createElement('script');
-	script.appendChild(document.createTextNode('('+ wrapper +')();'));
-	(document.body || document.head || document.documentElement).appendChild(script);
+  //---------------------------------------------------------------------------------------
+  // Append the stylesheet
+  //---------------------------------------------------------------------------------------
+  window.plugin.decayCalculator.setupCSS = function(){
+    $('<style>').prop('type', 'text/css').html(''
+      +'.decayCalculator{text-align:center;padding:2px 0 3px;border:1px solid #20A8B1;border-width:1px 0;}'
+      +'.decayCalculator div{display:inline-block;width:33%;color:lightgrey;}'
+      +'.decayCalculator div b{color:#ffce00;}'
+    ).appendTo('head');
+  }
+
+  var setup = function() {
+    window.plugin.decayCalculator.setupCSS();
+
+    window.addHook('portalDetailsUpdated', window.plugin.decayCalculator.appendDetails);
+  };
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+
+setup.info = plugin_info; //add the script info data to the function as a property
+if(!window.bootPlugins) window.bootPlugins = [];
+window.bootPlugins.push(setup);
+// if IITC has already booted, immediately run the 'setup' function
+if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
